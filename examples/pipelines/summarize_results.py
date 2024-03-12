6e71abd994567f3beceb0d2a8ab5f77b4e179376
Ritesh <rshah@redhat.com>
2024-03-12 16:12:06 +0530

    Updated for pipelines

--- examples/pipelines/summarize_results.py
+++ examples/pipelines/summarize_results.py
@@ -2,10 +2,8 @@ import json
 
 def summarize_results():
     results = {}
-    with open('responsetime_result.json') as f1, open("security_result.json") as f2, open("quality_result.json") as f3:
+    with open('responsetime_result.json') as f1:
         results.update(json.load(f1))
-        results.update(json.load(f2))
-        results.update(json.load(f3))
 
     print(results)
     
